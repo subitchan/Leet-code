133fc1ff91d4167ac58de377d6082514b8875cdb
subitchan <74167727+subitchan@users.noreply.github.com>
2024-08-04 10:13:54 +0530

    Time: 4 ms (40.27%) | Memory: 45.7 MB (22.24%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-          Arrays.sort(nums);
+         Arrays.sort(nums);
           int res[]=new int[2];
           int x=0;
           for(int i=0;i<nums.length-1;i++)
